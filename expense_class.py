de1daf41d1da7f4b6d3084421804bbda3faf2822
*Ezekiel <*ezekieloshin@gmail.com*>
2023-12-18 09:04:46 +0100

    added random module. Learning further.

--- expense_class.py
+++ expense_class.py
@@ -2,6 +2,7 @@
 
 from uuid import uuid4
 from datetime import datetime, timezone
+import random
 
 class Expense:
     def __init__(self, title, amount):
@@ -27,13 +28,33 @@ class Expense:
             'updated_at': self.updated_at.isoformat()
         }
 
+# Predefined expense titles
+predefined_titles = ["Groceries", "Furnitures", "Electronics", "Clothing", "Books", "Home Decor", "Appliances", "Toys", "Sports Gear", "Stationery"]
+
+# Generate up to 20 randomly created Expense instances
+expenses = []
+for _ in range(20):
+    title = random.choice(predefined_titles)
+    amount = round(random.uniform(1.0, 100.0), 2)
+    expense = Expense(title, amount)
+    expenses.append(expense)
+
+# Print the generated expenses
+for i, expense in enumerate(expenses, start=1):
+    print(f"Expense {i}: {expense.to_dict()}")
+
 # Sample usage:
 expense1 = Expense("Groceries", 50.0)
+expense2 = Expense("Utensils", 60.0)
+
 print(expense1.to_dict())
+print(expense2.to_dict())
 
 # Updating the expense
 expense1.update(amount=55.0)
 print(expense1.to_dict())
+
+
 
 
 
